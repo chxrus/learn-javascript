351d044bdc3456147855a94eae6d1b6acd871104
chxrus-fedora <lukovkin.ark@gmail.com>
2023-07-14 16:41:03 +0300

    Add 14.5.js

---
+++ 1. Javascript/14.5.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// #2
+
+function isInteger(num) {
+  return num === ~~num;
+}
+
+console.log(isInteger(1)); // true
+console.log(isInteger(1.5)); // false
+console.log(isInteger(-0.5)); // false
